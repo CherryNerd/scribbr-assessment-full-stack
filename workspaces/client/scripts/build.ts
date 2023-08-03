05814912fe0815b988ef8b046fa8a1dcb9777223
3T | Renzo <renzo@3tdev.nl>
2023-08-03 13:21:31 +0200

    build(client): move from abandoned esbuild-plugin-postcss2 to esbuild-style-plugin, add tailwindcss

--- workspaces/client/scripts/build.ts
+++ workspaces/client/scripts/build.ts
@@ -1,43 +1,49 @@
 import esbuild from "esbuild";
-const postCssPlugin = require("esbuild-plugin-postcss2");
+
+const postCssPlugin = require("esbuild-style-plugin");
+
 
 type BuildOptions = {
-  env: "production" | "development";
+    env: "production" | "development";
 };
 
 export async function createContext(options: BuildOptions) {
-  const { env } = options;
+    const {env} = options;
 
-  return await esbuild.context({
-    entryPoints: ["./src/index.tsx"], // We read the React application from this entrypoint
-    outfile: "./public/index.js", // We output a single file in the public/ folder (remember that the "script.js" is used inside our HTML page)
-    define: {
-      "process.env.NODE_ENV": `"${env}"`, // We need to define the Node.js environment in which the client is built
-    },
-    plugins: [postCssPlugin.default()],
-    bundle: true,
-    minify: env === "production",
-    sourcemap: env === "development",
-  });
+    return await esbuild.context({
+        entryPoints: ["./src/index.tsx"], // We read the React application from this entrypoint
+        outfile: "./public/index.js", // We output a single file in the public/ folder (remember that the "script.js" is used inside our HTML page)
+        define: {
+            "process.env.NODE_ENV": `"${env}"`, // We need to define the Node.js environment in which the client is built
+        },
+        plugins: [postCssPlugin({
+            postcss: {
+                plugins: [require("tailwindcss"), require("autoprefixer")],
+            }
+        })],
+        bundle: true,
+        minify: env === "production",
+        sourcemap: env === "development",
+    });
 }
 
 async function dev() {
-  const context = await createContext({ env: "development" });
+    const context = await createContext({env: "development"});
 
-  await context.watch();
+    await context.watch();
 
-  console.log("Esbuild rebuilds the client on every file change");
+    console.log("Esbuild rebuilds the client on every file change");
 }
 
 async function build() {
-  const context = await createContext({ env: "production" });
+    const context = await createContext({env: "production"});
 
-  await context.dispose();
+    await context.dispose();
 }
 
 const args = process.argv.slice(2);
 if (args[0] === "dev") {
-  dev();
+    dev();
 } else {
-  build();
+    build();
 }
--- workspaces/client/src/App.tsx
+++ workspaces/client/src/App.tsx
@@ -1,12 +1,11 @@
 import { APP_TITLE } from "@scribbr-assessment-full-stack/common";
 import React, { useState } from "react";
-
-import "./index.css";
+import './index.css';
 
 export function App() {
   return (
     <div>
-      <h1>Welcome to {APP_TITLE}!</h1>
+      <h1 className={"text-3xl font-bold underline"}>Welcome to {APP_TITLE}!</h1>
       <p>This would be probably the starting point of your app.</p>
     </div>
   );
